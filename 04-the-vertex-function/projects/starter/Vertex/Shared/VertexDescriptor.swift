d186d55492ec56d07b938404a32f71d7b8664f83
Jinwoo Kim <kidjinwoo@me.com>
2022-08-31 16:56:05 +0900

    no message

--- 04-the-vertex-function/projects/starter/Vertex/Shared/VertexDescriptor.swift
+++ 04-the-vertex-function/projects/starter/Vertex/Shared/VertexDescriptor.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2022 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,43 +30,22 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-#include <metal_stdlib>
-using namespace metal;
+import MetalKit
 
-//vertex float4 vertex_main(constant packed_float3 *vertices [[buffer(0)]], constant ushort *indices [[buffer(1)]], constant float &timer [[buffer(11)]], uint vertexID [[vertex_id]]) {
-////  return float4(0, 0, 1, 1);
-//    ushort index = indices[vertexID];
-//    float4 position = float4(vertices[index], 1);
-//    position.y += timer;
-//    return position;
-//}
-
-struct VertexIn {
-    float4 position [[attribute(0)]];
-    float4 color [[attribute(1)]];
-};
-
-struct VertexOut {
-    float4 position [[position]];
-    float4 color;
-    float pointSize [[point_size]];
-};
-
-vertex VertexOut vertex_main(VertexIn in [[stage_in]], constant float &timer [[buffer(11)]]) {
-    float4 position = float4(in.position.x,
-                             in.position.y + timer,
-                             in.position.z,
-                             in.position.w);
-    VertexOut out {
-        .position = position,
-        .color = in.color,
-        .pointSize = 30
-    };
-    
-    return out;
-}
-
-fragment float4 fragment_main(VertexOut in [[stage_in]]) {
-//  return float4(0, 0, 1, 1);
-    return in.color;
+extension MTLVertexDescriptor {
+    static var defaultLayout: MTLVertexDescriptor {
+        let vertexDescriptor = MTLVertexDescriptor()
+        vertexDescriptor.attributes[0].format = .float3
+        vertexDescriptor.attributes[0].offset = 0
+        vertexDescriptor.attributes[0].bufferIndex = 0
+        
+//        vertexDescriptor.attributes[1].format = .float3
+//        vertexDescriptor.attributes[1].offset = 0
+//        vertexDescriptor.attributes[1].bufferIndex = 1
+        
+        vertexDescriptor.layouts[0].stride = MemoryLayout<Float>.stride * 3
+//        vertexDescriptor.layouts[1].stride = MemoryLayout<simd_float3>.stride
+        
+        return vertexDescriptor
+    }
 }
